ffcb3c2d538213b91d0f179bbebb027c118000a1
alien-bk <116715754+alien-bk@users.noreply.github.com>
2022-10-27 09:32:16 +0200

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -32,5 +32,30 @@ function braddy(){
         }else {document.getElementById("help").innerHTML = (`Since you are ${personAge} brad would consider you legal and too old so he's not interested.`)}
     } 
  
+function underAge(){
+    if (personAge <= 17) {
+        const geoFindMe = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, error, geoOptions);
+    } else {
+        console.log("Geolocation services are not supported by your web browser.");
+    }
+}
 
+const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const altitude = position.coords.altitude;
+    const accuracy = position.coords.accuracy;
+    console.log(`lat: ${latitude} long: ${longitude}`);
+}
 
+const error = (error) => {
+    console.log(`Unable to retrieve your location due to ${error.code}: ${error.message}`);
+}
+
+const geoOptions = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 27000
+} } };
